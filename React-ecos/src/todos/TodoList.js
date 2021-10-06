c900d1f863bf61b9b55f585b4ac8a2391b489d0d
Oliver Partida <oliver.partida@sage.com>
2021-10-06 17:38:13 +0200

    Dealing with side effects I

--- React-ecos/src/todos/TodoList.js
+++ React-ecos/src/todos/TodoList.js
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import TodoListItem from './TodoListItem';
 import NewTodoForm from "./NewTodoForm";
 import { connect } from 'react-redux';
 import { markTodoAsCompleted, removeTodo } from "./actions";
+import { loadTodos, displayAlert } from './thunks';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        { todos.map((todo, index) => <TodoListItem
-        key={index}
-        todo={todo} 
-        onRemovePressed={onRemovePressed} 
-        onCompletedPressed={onCompletedPressed} />)}
-    </div>
-);
+
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, []);
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
+        <div className="list-wrapper">
+            <NewTodoForm />
+            { todos.map((todo, index) => <TodoListItem
+            key={index}
+            todo={todo} 
+            onRemovePressed={onRemovePressed} 
+            onCompletedPressed={onCompletedPressed} />)}
+        </div>
+    )
+    return isLoading ? loadingMessage : content;
+};
 
 const mapStateToProps = state => ({
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch => ({
     onRemovePressed: text => dispatch(removeTodo(text)),
-    onCompletedPressed: text => dispatch(markTodoAsCompleted(text))
+    onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+    onDisplayAlertClicked: text => dispatch(displayAlert(text)),
+    startLoadingTodos: () => dispatch(loadTodos()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
